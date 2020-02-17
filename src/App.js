64a40464a3a62e51c0b4a76657b02172c72ed2fe
bgh331 <58461569+bgh331@users.noreply.github.com>
2020-02-17 18:05:37 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  const [todo, setTodo] = React.useState({ desc: "", date: "" });
+  const [todos, setTodos] = React.useState([]);
+
+  const inputChanged = (event) => {
+    setTodo({ ...todo, [event.target.name]: event.target.value });
+  }
+
+
+  const addTodo = (event) => {
+    event.preventDefault();
+    setTodos([...todos, todo]);
+  }
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>Todo List</h2>
+        <form onSubmit={addTodo}>
+          Description:
+        <input type="text" name="desc" value={todo.desc} onChange={inputChanged} />
+
+          Date:
+        <input type="date" name="date" value={todo.date} onChange={inputChanged} />
+          <input type="submit" value="Add" />
+        </form>
+      </header>
+      <table>
+        
+        <tbody>
+          <tr>
+            <th>Description: </th>
+            <th>Date: </th>
+          </tr>
+          {
+            todos.map((todo, index) =>
+              <tr key={index}>
+                <td>{todo.desc}</td>
+                <td>{todo.date}</td>
+              </tr>
+            )
+          }
+        </tbody>
+
+      </table>
+    </div>
+  );
+}
+
+export default App;
